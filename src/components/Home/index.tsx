0be6355aa4265a0947fed0d26790c61c7d62330a
unknown <hamiltonjlewis@gmail.com>
2020-11-13 00:25:18 -0600

    messed around with homepage

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,35 +1,27 @@
 import React from "react";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import {
+  makeStyles,
+  createStyles,
+  Theme
+} from "@material-ui/core/styles";
+import { CssBaseline } from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      minHight: '100vh',
+      backgroundImage: `url(${process.env.PUBLIC_URL + "/home.jpg"})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    }
+  }));
 
 const Home = () => {
+  const classes = useStyles();
   return (
-    <Card>
-      <CardActionArea>
-        <CardMedia
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Featured Video
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <div className={classes.root}><CssBaseline /><div>
+      <h1>Hello World</h1>
+    </div></div>
   )
 }
 export default Home;
